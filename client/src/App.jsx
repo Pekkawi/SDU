ab3de6e4ef479e436e9877599a8552eca97632f6
unknown <liehn.eduard1@gmail.com>
2023-06-24 19:09:44 +0300

    added dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,13 +32,13 @@ import { LinkTemplate } from "./components/LinkTemplate";
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white m-0 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] max-h-[7vh] sticky">
+      <header className="w-full flex justify-between items-center bg-white m-0 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] min-h-[10vh] sticky">
         <h1 className="text-[1.5em]">SDU - The Core</h1>
         <div className="flex">
           <h1 className="mr-2 px-4 py-2"> Random Name</h1>
           <button
-            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100
-           font-medium rounded-md text-sm  mr-2 px-4 py-2"
+            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-200
+           font-medium rounded-md text-sm  mr-2 px-6 py-2 sm:py-1"
           >
             Log in
           </button>
@@ -46,7 +46,7 @@ const App = () => {
       </header>
 
       <main className="m-0 p-0 w-full bg-[#f9fafe] min-h-[93vh] flex">
-        <div className="col-span-1 w-[185px] bg-white border-r border-r-[#e6ebf4] sticky">
+        <div className="col-span-1 w-[185px] min-w-[185px] bg-white border-r border-r-[#e6ebf4] sticky">
           <LinkTemplate text="Dashboard" source={dashboard} thePath="/" />
           <LinkTemplate text="Machines" source={machines} thePath="/machines" />
           <LinkTemplate text="Profile" source={profile} thePath="/profile" />
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="px-8 pt-8 pr-8 sm:px-6 pb-6 border-2 rounded-lg border-[#e6ebf4] bg-white m-2">
+      <h1 className="font-extrabold text-[#222328] text-[2rem]">
+        About this page
+      </h1>
+      <p className="mt-6 text-[#666e75] text-[1.2rem]">
+        This is the official webpage of the student workshop at SDU Sønderborg.
+        It offers a booking system for all manufacturing devices available. To
+        be able to use this system, you need to log in with your SDU user
+        account.
+      </p>
+      <button
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-200
+           font-medium rounded-md text-sm  mr-2 px-6 py-2 mt-6"
+      >
+        Log in
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
