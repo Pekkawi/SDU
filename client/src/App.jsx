6bfc90ff59310d715c9c69d92c108d3bb5056c35
unknown <liehn.eduard1@gmail.com>
2023-06-24 13:57:33 +0300

    succes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,31 +33,29 @@ const App = () => {
   return (
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white m-0 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] max-h-[7vh] sticky">
-        <h1 className="text-[2em]">SDU - The Core</h1>
+        <h1 className="text-[1.5em]">SDU - The Core</h1>
         <div>
           <h1> Log in </h1>
         </div>
       </header>
 
-      <main className="m-0 p-0 w-full bg-[#f9fafe] min-h-[93vh] grid grid-cols-8 gap-0 ">
-        <div className="col-span-1 bg-white border-r border-r-[#e6ebf4] pl-2">
-          <Link className="flex my-5 items-stretch">
-            <img src={dashboard} className="w-5 h-5 mr-3 ml-1" />
-            <h2>Dashboard</h2>
-          </Link>
-          <Link className="flex my-5 items-stretch">
-            <img src={machines} className="w-5 h-5 mr-3 ml-1 " />
-            <h2>Machines</h2>
-          </Link>
-          <Link className="flex my-5 items-stretch">
-            <h2>Profile</h2>
-          </Link>
-          <Link className="flex my-5 items-stretch">
-            <h2>Users</h2>
-          </Link>
+      <main className="m-0 p-0 w-full bg-[#f9fafe] min-h-[93vh] grid grid-cols-2 gap-0 ">
+        <div className="col-span-1 w-[185px] bg-white border-r border-r-[#e6ebf4] ml-0 sticky">
+          <LinkTemplate text="Dashboard" source={dashboard} thePath="/" />
+          <LinkTemplate text="Machines" source={machines} thePath="/machines" />
+          <LinkTemplate text="Profile" source={profile} thePath="/profile" />
+          <LinkTemplate text="Users" source={users} thePath="/users" />
+          <LinkTemplate text="Nodes" source={nodes} thePath="/nodes" />
+          <LinkTemplate
+            text="Permissions"
+            source={permissions}
+            thePath="/permissions"
+          />
+          <LinkTemplate text="Feedback" source={feedback} thePath="/feedback" />
+          <LinkTemplate text="Help" source={help} thePath="/help" />
         </div>
 
-        <div className="col-span-7">
+        <div className="col-span-1 w-auto">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/feedback" element={<Feedback />} />
--- client/src/components/LinkTemplate.jsx
+++ client/src/components/LinkTemplate.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const LinkTemplate = ({ text, source }) => {
+export const LinkTemplate = ({ text, source, thePath }) => {
   return (
     <>
-      <img src={source} className="w-5 h-5 mr-3 ml-1" />
-      <h2>{text}</h2>
+      <Link
+        className="flex py-4 border-y border-y-[#e6ebf4] hover:bg-gray-100 pl-4"
+        to={thePath}
+      >
+        <img src={source} className="w-5 h-5 mr-5" />
+        <h2>{text}</h2>
+      </Link>
     </>
   );
 };
