15d1ae1ffdbde7575ad83023d28b6575f633510d
unknown <liehn.eduard1@gmail.com>
2023-06-25 20:54:52 +0300

    something

--- client/src/pages/Nodes.jsx
+++ client/src/pages/Nodes.jsx
@@ -4,8 +4,8 @@ function Nodes() {
   return (
     <div className="px-8 pt-8 pr-8 sm:px-6 pb-6 border-2 rounded-lg border-[#e6ebf4] bg-white m-2">
       <TitleDescription
-        header="Feature request or bug report"
-        description="Tell us how we can improve the software"
+        header="Nodes"
+        description="Connected nodes for machine control"
       />
     </div>
   );
